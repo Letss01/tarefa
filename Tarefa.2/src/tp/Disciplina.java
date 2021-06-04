b0310f539ac8f52711f365692d1cb33321d5ed18
Leticia <Leticia@DESKTOP-GP6OAJN>
2021-06-04 06:38:47 -0300

    nov

--- Tarefa.2/src/tp/Disciplina.java
+++ Tarefa.2/src/tp/Disciplina.java
@@ -2,7 +2,48 @@ package tp;
 
 public class Disciplina {
 	
+	private double mediaN;
 	Aluno[] alunos = new Aluno[10];
+	
+	public void addAlu(int novPos, String novNome, 
+			int novId, double novN1, double novN2) {
+		
+		alunos[novPos].nota1 = novN1;
+		alunos[novPos].nota2 = novN2;
+		
+		alunos[novPos].alterarNome(novNome);
+		alunos[novPos].alteraId(novId);
+	}
+	
+	public double mediaN1() {
+		mediaN = 0;
+		for(int i = 0; i < alunos.length; i++) {
+			mediaN += alunos[i].nota1;
+		} return mediaN /10;
+		}
+	public double mediaN2() {
+		mediaN = 0;
+		for(int i = 0; i < alunos.length; i++) {
+			mediaN += alunos[i].nota2;
+		} return mediaN /10;
+		}
+	public void antEsuc (int novPos) {
+		if (novPos == 1) {
+			System.out.println(alunos[novPos].imprimeNome());
+		
+		}
+		else if(novPos == 10) {
+			System.out.println(alunos[novPos -2].imprimeNome());
+		}
+		else {
+			System.out.println(alunos[novPos -2].imprimeNome());
+			
+			System.out.println(alunos[novPos].imprimeNome());
+		}
+	
+	
+	}
+	
 
 
 }
